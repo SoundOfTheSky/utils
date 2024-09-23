5709cb838d250fd653bfa0bcb04043f71afab253
nikita.tkachiov <nikita.tkachiov@edgecenter.ru>
2024-09-23 23:02:22 +0300

    Types for manipulating strings

--- types.ts
+++ types.ts
@@ -9,3 +9,25 @@ export type JSONSerializable =
   | undefined
   | { [key: string]: JSONSerializable }
   | JSONSerializable[];
+
+export type ObjectAddPrefix<T, P extends string> = {
+  [K in keyof T as `${P}${string & K}`]: T[K];
+};
+
+export type CamelToSnakeCase<S extends string> = S extends `${infer T}${infer U}`
+  ? U extends Uncapitalize<U>
+    ? `${Lowercase<T>}${CamelToSnakeCase<U>}`
+    : `${Lowercase<T>}_${CamelToSnakeCase<Uncapitalize<U>>}`
+  : S;
+
+export type ObjectCamelToSnakeCase<T> = {
+  [K in keyof T as CamelToSnakeCase<string & K>]: T[K];
+};
+
+export type SnakeToCamel<S extends string> = S extends `${infer T}_${infer U}`
+  ? `${T}${Capitalize<SnakeToCamel<U>>}`
+  : S;
+
+export type ObjectSnakeToCamel<T> = {
+  [K in keyof T as SnakeToCamel<string & K>]: T[K];
+};
